cb0a2c78ef3ee653f8d35b253bfa5f8bbdd724ee
bharath <barathk777@gmail.com>
2024-09-20 17:57:59 +0530

    2nd change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import './App.css';
 import { Header } from './Components/index';
 import { Footer } from './Footer';
-import { CartCard } from './Components/CartCard';
 import { AllRoutes } from './Routes/AllRoutes';
 
 function App() {
